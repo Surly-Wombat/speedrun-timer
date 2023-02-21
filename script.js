53911c2b65cc1f58628204eb1e2f809a55ad4ea0
Surly-Wombat <113541817+Surly-Wombat@users.noreply.github.com>
2023-02-21 16:40:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const splitsContainer = document.getElementById("splits");
+const splits = splitsContainer.children;
+const sobDisplay = document.getElementById("sobTotal");
+const timeDisplay = document.getElementById("runningTime");
+let bestTimes = [];
+let pbTimes = [];
+for(let i = 0; i < splits.length; i++) {
+    bestTimes[i] = -1;
+    pbTimes[i] = -1;
+}
+let currentSegment = 0;
+
+function timeString(ms) {
+    if(ms < 0) {
+        return "0.00";
+    }
+    
+    let hours = Math.floor(ms / (60 * 60 * 1000));
+    let minutes = Math.floor((ms % (60 * 60 * 1000)) / (60 * 1000));
+    let seconds = Math.floor((ms % (60 * 1000)) / 10000);
+    ms = ms % 1000;
+    
+    minutes = (minutes < 10 && hours > 0) ? "0" + minutes : minutes;
+    seconds = (seconds < 10 && minutes > 0) ? "0" + seconds : seconds;
+    ms = (ms < 10) ? "0" + ms : ms;
+    
+    if(hours > 0) {
+        return `${hours}:${minutes}:${seconds}.${ms}`;
+    }
+    else if(minutes > 0) {
+        return `${minutes}:${seconds}.${ms}`;
+    }
+    else if(seconds > 0) {
+        return `${seconds}.${ms}`;
+    }
+    else {
+        return `0.${ms}`;
+    }
+}
+
+
 function scrollParentToChild(parent, child) {
     var parentRect = parent.getBoundingClientRect();
     var parentViewableArea = {
@@ -13,7 +54,8 @@ function scrollParentToChild(parent, child) {
           const scrollBot = childRect.bottom - parentRect.bottom;
           if (Math.abs(scrollTop) < Math.abs(scrollBot)) {
               parent.scrollTop += scrollTop;
-          } else {
+          } 
+          else {
               parent.scrollTop += scrollBot;
           }
     }
