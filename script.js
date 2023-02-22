556376cec8d000d7de6f9b89e1996e581637e04e
Surly-Wombat <113541817+Surly-Wombat@users.noreply.github.com>
2023-02-22 16:44:16 -0500

    UPDATE

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const timeDisplay = document.getElementById("runningTime");
 const splitButton = document.getElementById("split");
 let bestTimes = new Array(splits.length).fill(-1);
 let pbTimes = new Array(splits.length).fill(-1);
+for(let i = 0; i < pbTimes.length; i++) {
+    pbTimes[i] = 4000 * (i+1);
+}
 
 let currentTimes = [];
 let currentSegment = 0;
@@ -33,17 +36,48 @@ function updateTimer() {
 }
 
 function splitTimer() {
-    bestTimes[currentSegment] = Math.max(bestTimes[currentSegment],segmentTime);
+    currentTimes[currentSegment] = elapsedTime;
     lastSegmentTime += segmentTime;
+    splitCompare();
+    bestTimes[currentSegment] = Math.min(bestTimes[currentSegment],segmentTime);
     segmentTime = 0;
     currentSegment++;
     changeCurrent(splits[currentSegment]);
+    scrollParentToChild(splits,splits[currentSegment]);
 }
 
 function changeCurrent(element) {
     runningSegment.querySelector(".splitIcon").innerHTML = element.querySelector(".splitIcon").innerHTML;
     runningSegment.querySelector(".splitName").innerHTML = element.querySelector(".splitName").innerHTML;
     runningSegment.querySelector(".splitTime").innerHTML = element.querySelector(".splitTime").innerHTML;
+}
+
+function splitCompare() {
+    const currentSplit = splits[currentSegment];
+    const splitDiffDisplay = currentSplit.querySelector(".splitDiff");
+    if(pbTimes[currentSegment] == -1) return;
+    let totalDiff = elapsedTime - pbTimes[currentSegment];
+    let splitDiff;
+    if(currentSegment == 0) {
+        splitDiff = totalDiff;
+        splitDiffDisplay.innerHTML = timeDiff(totalDiff);
+    }
+    else {
+        let pbSplit = pbTimes[currentSegment] - pbTimes[currentSegment-1];
+        splitDiff = segmentTime - pbSplit;
+        splitDiffDisplay.innerHTML = timeDiff(totalDiff);
+    }
+    if(segmentTime < bestTimes[currentSegment]) {
+        currentSplit.classList.add("new-best");
+    }
+    else if(splitDiff > 0) {
+        if(elapsedTime > pbTimes[currentSegment]) currentSplit.classList.add("slow-behind");
+        else currentSplit.classList.add("slow-ahead");
+    }
+    else {
+        if(elapsedTime > pbTimes[currentSegment]) currentSplit.classList.add("fast-behind");
+        else currentSplit.classList.add("fast-ahead");
+    }
 }
 
 function timeString(ms) {
@@ -74,6 +108,16 @@ function timeString(ms) {
     }
 }
 
+function timeDiff(time) {
+    let absTime = Math.abs(time);
+    if(time > 0) {
+        return `+${timeString(absTime)}`;
+    }
+    else {
+        return `-${timeString(absTime)}`;
+    }
+}
+
 
 function scrollParentToChild(parent, child) {
     var parentRect = parent.getBoundingClientRect();
@@ -98,3 +142,17 @@ function scrollParentToChild(parent, child) {
 }
 
 splitButton.addEventListener("click", startTimer);
+
+window.onload = function() {
+    if(typeof(localStorage.bestTimes) !== "undefined") {
+        bestTimes = localStorage.bestTimes;
+    }
+    if(typeof(localStorage.pbTimes) !== "undefined") {
+        pbTimes = localStorage.pbTimes;
+    }
+}
+
+window.onbeforeunload = function() {
+    localStorage.bestTimes = bestTimes;
+    localStorage.pbTimes = pbTimes;
+}
